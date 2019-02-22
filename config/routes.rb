e47ca89b156dd3692bc56274bcc0b402889a96e9
Ivan <ivanvlora@gmail.com>
2019-02-22 17:15:34 +0900

    added route to destroy cocktails. Added button and styles for the delete icon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: %i[index show new create] do
+  resources :cocktails, only: %i[index show new create destroy] do
     resources :doses, only: %i[new create]
   end
   resources :doses, only: [:destroy]
